b81bdf565b6e0cbea0843155c85ebe523cf2ec78
MahmoudDawood <mm.dawood16@yahoo.com>
2023-09-23 19:14:05 +0300

    Add error handler middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
-import asyncHandler from "express-async-handler";
+// import asyncHandler from "express-async-handler";
 import helmet from "helmet";
 import morgan from "morgan";
 import { initDb } from "./database";
+import errorHandler from "./middlewares/errorHandler";
+import { bakerRouter, memberRouter, orderRouter, productRouter } from "./routers/";
 require("dotenv").config();
 
 (async () => {
@@ -16,9 +18,16 @@ require("dotenv").config();
 	app.use(morgan("dev"));
 	app.use(helmet());
 
+	app.use("/bakers", bakerRouter);
+	app.use("/members", memberRouter);
+	app.use("/products", productRouter);
+	app.use("/orders", orderRouter);
+
 	app.get("/", (req, res) => {
 		res.send("Hello world");
 	});
+
+	app.use(errorHandler);
 
 	app.listen(port, () => {
 		console.log(`Server is running on port ${port}`);
